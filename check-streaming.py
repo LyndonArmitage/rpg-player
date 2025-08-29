d5aeb88f13acd51ec04e319f748fa6b3b9bf6586
Lyndon Armitage <lyndon.armitage@gmail.com>
2025-08-29 14:49:42 +0100

    Add code to check streaming of gpt-5

---
+++ check-streaming.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+openai = OpenAI()
+
+response = openai.responses.create(model="gpt-5-mini", input="Hello world", stream=True)
+for chunk in response:
+    print(chunk)
