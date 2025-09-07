9cd7329f1aa7529cffdea7a32c029ac0e4bc778c
Lyndon Armitage <lyndon.armitage@gmail.com>
2025-09-07 19:22:52 +0100

    Add Jinja template parsing to OpenAIAgent

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import List, Optional, Union, override
 
+from jinja2 import Environment, Template
 from openai import OpenAI
 
 from chat_message import ChatMessage, ChatMessages
@@ -82,18 +83,29 @@ class OpenAIAgent(Agent):
             path = Path(path)
         if not path.exists():
             raise FileNotFoundError(f"{path} does not exist")
-        system_prompt: str = path.read_text()
+
+        # Create jinja environment and template variables to use
+        env = Environment()
+        template_vars: dict = {
+            "name": name,
+            "model": model,
+        }
+
+        system_prompt_template: Template = env.from_string(path.read_text("utf-8"))
+        system_prompt: str = system_prompt_template.render(**template_vars)
 
         if prefix_path:
             if not isinstance(prefix_path, Path):
                 prefix_path: Path = Path(prefix_path)
-            prefix_prompt: str = prefix_path.read_text()
+            prefix_template: Template = env.from_string(prefix_path.read_text("utf-8"))
+            prefix_prompt: str = prefix_template.render(**template_vars)
             system_prompt = prefix_prompt + "\n" + system_prompt
 
         if suffix_path:
             if not isinstance(suffix_path, Path):
                 suffix_path: Path = Path(suffix_path)
-            suffix_prompt: str = suffix_path.read_text()
+            suffix_template: Template = env.from_string(suffix_path.read_text("utf-8"))
+            suffix_prompt: str = suffix_template.render(**template_vars)
             system_prompt = system_prompt + "\n" + suffix_prompt
 
         return OpenAIAgent(
